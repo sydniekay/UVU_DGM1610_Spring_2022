3d05af78fd5cd1ee5fde6a26138b3dfc724926ca
Sydnie Corey <sydnie.kaylene@gmail.com>
2022-02-16 21:19:09 -0700

    Made a new project for 1610 scripting practice, and created variables.cs

---
+++ 1610 Scripting Practice/Variables.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1610_Scripting_Practice
+{
+    internal class Variables
+    {
+        static void Main(string[] args)
+        {
+            // a list of variables with examples
+            int myInt = 12;
+            double myDecimal = 12.25;
+            char myCharacter = 'H';
+            string myString = "Happy";
+            bool myBool = true;
+
+
+            // using variables
+            Console.WriteLine($"{myInt}+{myDecimal} is {myInt + myDecimal}.");
+            Console.WriteLine($"{myString} starts with {myCharacter}.");
+            Console.WriteLine($"Are you feeling {myString} today?");
+            string response = Console.ReadLine();
+            
+            if (response.ToLower() == "yes")
+            {
+                Console.WriteLine("I'm glad to hear that!");
+            }
+            else
+            {
+                Console.WriteLine("I'm sorry to hear that.");
+            }
+
+            while (true) ;
+
+        }
+    }
+}
