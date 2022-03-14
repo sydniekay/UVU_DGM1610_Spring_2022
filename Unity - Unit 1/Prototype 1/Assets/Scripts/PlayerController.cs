993ed4c28c52568bf26510a2b0c1ec99de6b640b
Sydnie Corey <sydnie.kaylene@gmail.com>
2022-03-13 21:00:51 -0600

    Finished Lesson 1.4 - Step Into the Driver's Seat

--- Unity - Unit 1/Prototype 1/Assets/Scripts/PlayerController.cs
+++ Unity - Unit 1/Prototype 1/Assets/Scripts/PlayerController.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
-    public float speed = 5.0f;
+    // Private variables
+    private float speed = 20.0f;
+    private float turnSpeed = 50f;
+    private float horizontalInput;
+    private float forwardInput;
 
 
     // Start is called before the first frame update
@@ -17,8 +20,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Move the vehicle forward
-        //transform.Translate(0, 0, 1);
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        // Moves the car forward based on vertical input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // Rotates the car based on horizontal input
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+
     }
 }
