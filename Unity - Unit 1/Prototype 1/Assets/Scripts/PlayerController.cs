6d518357edd4051639c5ee6b143ab34470bc979b
Sydnie Corey <sydnie.kaylene@gmail.com>
2022-03-13 20:35:36 -0600

    Finished Lesson 1.3 - High Speed Chase

--- Unity - Unit 1/Prototype 1/Assets/Scripts/PlayerController.cs
+++ Unity - Unit 1/Prototype 1/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    public float speed = 5.0f;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +19,6 @@ public class PlayerController : MonoBehaviour
     {
         // Move the vehicle forward
         //transform.Translate(0, 0, 1);
-        transform.Translate(Vector3.forward * Time.deltaTime * 30);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
     }
 }
