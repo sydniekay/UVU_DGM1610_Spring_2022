af2381e12b9dc00a6923fb6f9db1e1fda653d48d
Sydnie Corey <sydnie.kaylene@gmail.com>
2022-04-12 20:48:11 -0600

    Completed Lessons 5.2 and 5.3

--- Unity - Unit 5/Prototype 5/Assets/Scripts/GameManager.cs
+++ Unity - Unit 5/Prototype 5/Assets/Scripts/GameManager.cs
@@ -1,16 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+    public Button restartButton;
+    public bool isGameActive;
     private float spawnRate = 1.0f;
-   
+    private int score;
+
+
     // Start is called before the first frame update
     void Start()
     {
+        isGameActive = true;
         StartCoroutine(SpawnTarget());
+        score = 0;
+        UpdateScore(0);
+
     }
 
     // Update is called once per frame
@@ -21,12 +34,31 @@ public class GameManager : MonoBehaviour
 
     IEnumerator SpawnTarget()
     {
-        while (true)
+        while (isGameActive)
         {
             yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0, targets.Count);
             Instantiate(targets[index]);
+
         }
 
+    }
+
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
+    }
+
+    public void GameOver()
+    {
+        restartButton.gameObject.SetActive(true);
+        gameOverText.gameObject.SetActive(true);
+        isGameActive = false;
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
--- Unity - Unit 5/Prototype 5/Assets/Scripts/Target.cs
+++ Unity - Unit 5/Prototype 5/Assets/Scripts/Target.cs
@@ -6,6 +6,7 @@ public class Target : MonoBehaviour
 {
 
     private Rigidbody targetRb;
+    private GameManager gameManager;
 
     private float minSpeed = 12;
     private float maxSpeed = 16;
@@ -13,11 +14,18 @@ public class Target : MonoBehaviour
     private float xRange = 4;
     private float ySpawnPos = -2;
 
+    public int pointValue;
+
+    public ParticleSystem explosionParticle;
+
 
     // Start is called before the first frame update
     void Start()
     {
         targetRb = GetComponent<Rigidbody>();
+
+        // Calling out another script called 'Game Manager'
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
 
         targetRb.AddForce(RandomForce(), ForceMode.Impulse);
 
@@ -34,12 +42,21 @@ public class Target : MonoBehaviour
 
     private void OnMouseDown()
     {
+        if(gameManager.isGameActive)
+        // When you click the mouse on an object, destroy it, activate explotion, and update player's score.
         Destroy(gameObject);
+
+        Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+        gameManager.UpdateScore(pointValue);
     }
 
     private void OnTriggerEnter(Collider other)
     {
         Destroy(gameObject);
+        if (!gameObject.CompareTag("Bad")) 
+        { 
+            gameManager.GameOver(); 
+        }  
     }
 
     // Separate methods made to call out in the start method.
