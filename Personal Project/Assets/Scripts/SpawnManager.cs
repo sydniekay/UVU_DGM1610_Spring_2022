a9f8800b9b08650d591273517b44afae23b05ef0
Sydnie Corey <sydnie.kaylene@gmail.com>
2022-04-15 19:48:09 -0600

    Worked on assets and game mechanics for Beetle Blast

--- Personal Project/Assets/Scripts/SpawnManager.cs
+++ Personal Project/Assets/Scripts/SpawnManager.cs
@@ -4,15 +4,69 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    public GameObject butterflyPrefab;
+    public GameObject beePrefab;
+    public GameObject ladybugPrefab;
+    public GameObject beetlePrefab;
+    public GameObject healthPrefab;
+
+    private float spawnRangeX = 6.5f;
+    private float spawnRangeY = 9;
+
+    public int waveNumber = 1;
+    public int insectCount;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        SpawnInsectWave(waveNumber);
+        SpawnHealth();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Increases the number of insects and powerups per wave
+        insectCount = FindObjectsOfType<Insects>().Length;
         
+        if (insectCount == 0)
+        {
+            waveNumber++;
+            SpawnInsectWave(waveNumber);
+
+            SpawnHealth();
+        }
+
+
+
+    }
+
+    // Random position within the range of the screen (going from bottom to top)
+    private Vector3 GenerateSpawnPosition()
+    {
+        float spawnPositionX = Random.Range(-spawnRangeX, spawnRangeX);
+        float spawnPositionY = Random.Range(- spawnRangeY, spawnRangeY);
+
+        Vector3 randomPosition = new Vector3(spawnPositionX, spawnPositionY, 0);
+
+        return randomPosition;
+    }
+
+    // Spawn insects in a wave
+    void SpawnInsectWave(int insectsToSpawn)
+    {
+        for (int i = 0; i < insectsToSpawn; i++)
+        {
+            Instantiate(beetlePrefab, GenerateSpawnPosition(), beetlePrefab.transform.rotation);
+        }
+    }
+
+    // Spawn health powerup
+    void SpawnHealth()
+    {
+        if (insectCount == 0)
+        {
+            Instantiate(healthPrefab, GenerateSpawnPosition(), healthPrefab.transform.rotation);
+        }
     }
 }
--- Personal Project/README.md
+++ Personal Project/README.md
@@ -15,6 +15,9 @@
 - Dying animations when objects collide
 - Health animation
 - UI stuff 
-### Features to Tweak:
-- Figure out SpawnManager
-- Tweak trigger collisions (bugs)
+### Features to Change:
+- Player controls switch to mouse click instead of arrow keys
+- Spray bottle moves with mouse, shoots projectile in the direction of the click (or we could just have a spray animation with the bottle, and an animation on the insect implying that it has been sprayed)
+- Insects come from all directions instead of just moving bottom to top
+- Edit trigger controls on objects
+
