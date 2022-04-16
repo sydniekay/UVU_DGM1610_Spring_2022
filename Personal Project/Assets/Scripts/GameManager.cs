2fcc64a80c9252b0b4c66e32b7adc7a5e9656101
Sydnie Corey <sydnie.kaylene@gmail.com>
2022-04-15 22:13:55 -0600

    Added comments in each script of where I want things to go

--- Personal Project/Assets/Scripts/GameManager.cs
+++ Personal Project/Assets/Scripts/GameManager.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    /* This script will contain the following functionalities:
+     * Spawning enemies (coroutine)
+     * Game over 
+     * UI stuff
+     * 
+     * Reference Prototypes 4 and 5
+     */
+
+
     // Start is called before the first frame update
     void Start()
     {
--- Personal Project/Assets/Scripts/PlayerController.cs
+++ Personal Project/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    /* This script will contain the following functionalities:
+     * Start of the game (call out Game Manager script/object)
+     * Collisions/Triggers
+     * Random Spawn position
+     * Mouse controls
+     * 
+     * Reference Prototypes 4 and 5
+     */
+    
     // Start is called before the first frame update
     void Start()
     {
