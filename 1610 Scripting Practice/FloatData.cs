2fc177e153770071c9fb3b14a51ae4838bfe8444
Sydnie Corey <sydnie.kaylene@gmail.com>
2022-04-21 15:15:04 -0600

    Created FloatData.cs and IntData.cs scripts

---
+++ 1610 Scripting Practice/FloatData.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+
+// Taking notes on scriptable objects based on Anthony's provided practice code
+
+[CreateAssetMenu]
+
+// We need to replace MonoBehavior with ScriptableObject in order for this to work
+public class FloatData : ScriptableObject
+{
+    public UnityEvent onZeroEvent;
+    public float value;
+
+
+    public void UpdateValue(float number)
+    {
+        value += number;
+    }
+
+    public void ReplaceValue(float number)
+    {
+        value = number;
+    }
+
+    public void DisplayImage(Image img)
+    {
+        if (value <= 0)
+        {
+            onZeroEvent.Invoke();
+        }
+        else if (value >= 0)
+        {
+            value = 1;
+        }
+
+        img.fillAmount = value;
+    }
+
+    public void DisplayNumber(Text txt)
+    {
+        // .ToString allows you to easily convert an int or float into a string.
+        txt.text = value.ToString();
+    }
+}
+
+/* Why do we have two of the same scripts for "FloatData" and "IntData"?
+ * My guess is that you'd want the asset to function the same, accept the data
+ * inside of the floats can be much more precise due to its ability to hold a decimal.
+ * Otherwise, they hold essentialy the same data and perform the same actions with it.
+ */
---
+++ 1610 Scripting Practice/IntData.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+
+// Taking notes on scriptable objects based on Anthony's provided practice code
+// You have to create the script as an asset because scriptable objects cannot be applied to game objects.
+[CreateAssetMenu]
+
+// We need to replace MonoBehavior with ScriptableObject in order for this to work
+public class IntData : ScriptableObject
+{
+    public UnityEvent onZeroEvent;
+    public float value;
+
+
+    public void UpdateValue(float number)
+    {
+        value += number;
+    }
+
+    public void ReplaceValue(float number)
+    {
+        value = number;
+    }
+
+    public void DisplayImage(Image img)
+    {
+        img.fillAmount = value;
+    }
+
+    public void DisplayNumber(Text txt)
+    {
+        // .ToString allows you to easily convert an int or float into a string.
+        txt.text = value.ToString();
+    }
+}
+
+/* Scriptable objects are not bound to a scene's run time, so its values can be edited during gameplay
+ * Another nice feature of scriptable objects is that instead of making copies of the data for each 
+ * instance on a prefab, for example, it stores one set of the data and you call back to it when invoked
