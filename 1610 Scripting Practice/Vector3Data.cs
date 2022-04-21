728d8fa3325b5bf2e020d6cbec604c8d2b1d65a0
Sydnie Corey <sydnie.kaylene@gmail.com>
2022-04-21 15:25:13 -0600

    Created Vector3Data.cs script

---
+++ 1610 Scripting Practice/Vector3Data.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+
+// Taking notes on scriptable objects based on Anthony's provided practice code
+
+[CreateAssetMenu]
+
+// We need to replace MonoBehavior with ScriptableObject in order for this to work
+public class Vector3Data : ScriptableObject
+{
+    public Vector3[] spawnPoint;
+
+    private void Start()
+    {
+        SpawnEntities();
+    }
+
+    private void SpawnEntities()
+    {
+        if (spawnPoints.transform.position.z = 0)
+        {
+            spawnPoint = new Vector3(0, 0, 6);
+        }
+    }
+
+    // Again, I'm doing this on my own in my IDE outside of Unity, so some of this is pseudo code...
+}
